017c3c44f7b309ab2f7f4b75e19c49df908d794a
Ilya-ilik <101934601+Ilya-ilik@users.noreply.github.com>
2023-02-19 23:51:36 +0300

    first commit initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
+//Чтобы использовать хук useState надо его сначала обьявить как показано ниже
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState([]);
+  // Тестовая функция первой кнопки
+  let testFunction1 = () => {
+    setValue(value * 10);
+  };
+   // Тестовая функция второй кнопки
+   let testFunction2 = () => {
+    setValue(value * 100);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Тестовая кнопка 1 */}
+      <button onClick={testFunction1}>Тестовая кнопка 1</button>
+      {/* Тестовая кнопка 2 */}
+      <button onClick={testFunction2}>Тестовая кнопка 2</button>
+
+      {/* Этот инпут сразу выводит вводимое из инпута */}
+      <input
+        type="text"
+        placeholder="Введите число"
+        onChange={(ivent) => setValue(ivent.target.value * 100)}
+      ></input>
+      {/* Тестовый вывод состояния можно удалить! */}
+      {value}
+      {/* Значение инпута это состояние value -  value={value} */}
+      <input value={value}></input>
     </div>
   );
 }
